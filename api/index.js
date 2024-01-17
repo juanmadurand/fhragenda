d6a3c397a7bd1e6fd65375496d28c9165e4c99be
Juanma Durand <juanma.durand@gmail.com>
2024-01-17 17:57:05 -0300

    Add Auth0

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-require("dotenv").config({ path: "./.env.local" });
+require('dotenv').config({ path: './.env.local' });
 
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
-const { auth } = require("express-oauth2-jwt-bearer");
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const {
   AUTH0_AUDIENCE,
@@ -14,9 +14,7 @@ const {
 } = process.env;
 
 if (!AUTH0_AUDIENCE || !AUTH0_ISSUER_BASE_URL || !AUTH0_BASE_URL) {
-  throw new Error(
-    "Please make sure that the file .env.local is in place and populated"
-  );
+  throw new Error('Please make sure that the file .env.local is in place and populated');
 }
 
 const checkJwt = auth({
@@ -27,21 +25,19 @@ const checkJwt = auth({
 app.use(cors({ origin: AUTH0_BASE_URL })); // allows requests from frontend
 app.use(helmet()); // just a good security practice
 
-app.get("/api/private", checkJwt, (req, res) => {
-  res.send({
-    msg: "Success",
-  });
+app.get('/api/contacts', checkJwt, (req, res) => {
+  res.send([{ id: 1, test: true }]);
 });
 
 // Global error handler outputs json
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(err.status || 500).json({
-    error: err.message || "Unknown error",
+    error: err.message || 'Unknown error',
   });
 });
 
 const server = app.listen(API_PORT, () =>
   console.log(`API Server listening on port ${API_PORT}`)
 );
-process.on("SIGINT", () => server.close());
+process.on('SIGINT', () => server.close());
---
+++ components/Navbar.js
@@ -0,0 +1,29 @@
+'use  client';
+
+import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+const NavBar = () => {
+  const { user, isLoading } = useUser();
+
+  const handleFetchContacts = async () => {
+    const response = await fetch('/api/contacts');
+    const contacts = await response.json();
+
+    console.log('contacts', contacts);
+  };
+
+  return (
+    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      {!isLoading && user && <div>Hello {user.name}</div>}
+      {!isLoading && !user && <a href="/api/auth/login">Log in</a>}
+      {user && (
+        <button type="button" onClick={handleFetchContacts}>
+          Fetch contacts
+        </button>
+      )}
+    </p>
+  );
+};
+
+export default NavBar;
